aa4d3cb6f448af258271398eebaa915d1e06cebb
César Pinto Castillo <cesar@jagcesar.se>
2016-12-07 12:19:51 +0100

    Adds some Unit tests

--- CloudKitCurrentUserTests/CloudKitCurrentUserTests.swift
+++ CloudKitCurrentUserTests/CloudKitCurrentUserTests.swift
@@ -10,27 +10,16 @@ import XCTest
 @testable import CloudKitCurrentUser
 
 class CloudKitCurrentUserTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testInit() {
+        XCTAssertNotNil(CurrentUser.sharedInstance)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testNotificationName() {
+        XCTAssertEqual(CurrentUser.statusChangedNotification.rawValue, "CurrentUserStatusChanged")
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
+    // I would love to add more Unit tests here, but since we can't enable CloudKit for the
+    // CloudKitCurrentUser target, we can't test any of the other public functions. Any ideas
+    // on how to solve this are welcome!
 }
